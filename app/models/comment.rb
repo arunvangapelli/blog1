020d12f2978e951ebce0407003bdcdbbf95b4075
Web Developer <username@Lubuntu-VirtualBox.(none)>
2012-10-04 00:48:48 -0600

    submit

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :body, :commenter, :post_id
+  belongs_to :post
+  validates :commenter, :presence => true
+  validates :post_id, :presence => true
+  validates :body, :presence => true
+                   
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog1::Application.config.secret_token = '0e7cd5f785f08c2a0621a4fef0bc5bb5abd4e1fdd5fbbfadcf3448437d1ee475b35bce088cfbe0508a7e1f76357f198c056ac930bc486c51483c3727267aa83a'
